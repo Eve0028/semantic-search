4cbaf30d13dd8273b379442fb1ed487941064ff7
Kacper Ledwon <you@example.com>
2023-12-17 17:44:35 +0100

    fix the gui

--- engine.py
+++ engine.py
@@ -2,7 +2,7 @@ import pandas as pd
 import dearpygui.dearpygui as dpg
 
 from search_engine.semantic_search import search
-from search_engine.parameters import TOKENIZER, VECTORIZER, SEMANTIZATOR, NUM_TOPICS, SIMILARITY_METRIC
+from search_engine.parameters import TOKENIZERS, VECTORIZERS, SEMANTIZATORS, NUM_TOPICS, SIMILARITY_METRICS
 
 # Read articles
 # TODO: Read articles from the database - Jakub
@@ -22,28 +22,45 @@ articles = pd.DataFrame(
 # Można w menu zaimplementować ich wybór
 # ewentualnie można też dodać opcję ponownego preprocessingu i dodania do bazy
 #    - jeśli dodamy dodatkowe artykuły do bazy/folderu
-tokenizer_choice = TOKENIZER
-vectorizer_choice = VECTORIZER
-semantic_method_choice = SEMANTIZATOR
-similarity_metric_choice = SIMILARITY_METRIC
-num_topic = NUM_TOPICS
+# tokenizer_choice = TOKENIZER
+# vectorizer_choice = VECTORIZER
+# semantic_method_choice = SEMANTIZATOR
+# similarity_metric_choice = SIMILARITY_METRIC
+# num_topic = NUM_TOPICS
+
+tokenizer_choice = TOKENIZERS[0]
+vectorizer_choice = VECTORIZERS[0]
+semantic_method_choice = SEMANTIZATORS[0]
+similarity_metric_choice = SIMILARITY_METRICS[0]
 
 
 def search_callback():
-    user_query = input("Podaj zapytanie: ")
+    user_query = dpg.get_value("QueryInput")
+
+    # Optional: Handle choices from the user for tokenization, vectorization, etc.
+    tokenizer_choice = dpg.get_value("TokenizerChoice")
+    vectorizer_choice = dpg.get_value("VectorizerChoice")
+    semantic_method_choice = dpg.get_value("SemantizatorChoice")
+    similarity_metric_choice = dpg.get_value("SimilarityMetricChoice")
 
     # Search
-    similarities = search(user_query, articles, tokenizer=tokenizer_choice, vector_method=vectorizer_choice,
-                          semantic_method=semantic_method_choice, similarity_metric=similarity_metric_choice,
-                          num_topics=num_topic)
+    similarities = search(
+        user_query,
+        articles,
+        tokenizer=tokenizer_choice,
+        vector_method=vectorizer_choice,
+        semantic_method=semantic_method_choice,
+        similarity_metric=similarity_metric_choice,
+        num_topics=NUM_TOPICS,
+    )
 
     # Add 'Similarity' column
-    articles['similarity'] = similarities
+    articles["similarity"] = similarities
 
     # Sort the articles by 'similarity' desc
-    articles_sorted = articles.sort_values(by='similarity', ascending=False)
+    articles_sorted = articles.sort_values(by="similarity", ascending=False)
 
-    # List articles from the best ones
+    # Update results in Dear PyGui
     result_text = "\n".join(articles_sorted["content"].tolist())
     dpg.set_value("ResultsOutput", result_text)
 
@@ -51,26 +68,27 @@ def search_callback():
 # Find best match article
 # best_match_index = similarities.index(max(similarities))
 # best_match_article = articles.iloc[best_match_index]['content']
-
+#
 # print("Najbardziej pasujący artykuł: ")
 # print(best_match_article)
 
-with dpg.create_viewport(title="Search Engine"):
-    with dpg.create_viewport(width=500, height=300, title="Search Engine"):
-        dpg.add_input_text(label="Query", id="QueryInput", width=200)
-        dpg.add_combo(label="Tokenizer", items=TOKENIZER, default_value=tokenizer_choice, id="TokenizerChoice")
-        dpg.add_combo(label="Vectorizer", items=VECTORIZER, default_value=vectorizer_choice, id="VectorizerChoice")
-        dpg.add_combo(label="Semantizator", items=SEMANTIZATOR, default_value=semantic_method_choice,
-                      id="SemantizatorChoice")
-        dpg.add_combo(label="Similarity Metric", items=SIMILARITY_METRIC, default_value=similarity_metric_choice,
-                      id="SimilarityMetricChoice")
-        dpg.add_button(label="Search", callback=search_callback)
-        dpg.add_text("Results:")
-        dpg.add_text("", id="ResultsOutput", wrap=500)
+dpg.create_context()
+dpg.create_viewport(title='Semantic Search', width=600, height=300)
+
+with dpg.window(label="Search Engine", width=600, height=300):
+    dpg.add_input_text(label="Query", tag="QueryInput", width=200)
+    dpg.add_combo(label="Tokenizer", items=TOKENIZERS, default_value=tokenizer_choice, tag="TokenizerChoice")
+    dpg.add_combo(label="Vectorizer", items=VECTORIZERS, default_value=vectorizer_choice, tag="VectorizerChoice")
+    dpg.add_combo(label="Semantizator", items=SEMANTIZATORS, default_value=semantic_method_choice,
+                  tag="SemantizatorChoice")
+    dpg.add_combo(label="Similarity Metric", items=SIMILARITY_METRICS, default_value=similarity_metric_choice,
+                  tag="SimilarityMetricChoice")
+    dpg.add_button(label="Search", callback=search_callback)
+    dpg.add_text("Results:")
+    dpg.add_text("", tag="ResultsOutput", wrap=500)
 
 # Run Dear PyGui
 dpg.setup_dearpygui()
 dpg.show_viewport()
-dpg.create_viewport(title="Another Window")
-dpg.show_viewport()
-dpg.cleanup_dearpygui()
+dpg.start_dearpygui()
+dpg.destroy_context()
