f0ada8e36422a53d84b475e3f43a775f4d528554
Kacper Ledwon <you@example.com>
2023-12-17 17:52:40 +0100

    fix the gui

--- engine.py
+++ engine.py
@@ -12,22 +12,6 @@ from search_engine.parameters import TOKENIZERS, VECTORIZERS, SEMANTIZATORS, NUM
 articles = pd.DataFrame(
     {'content': ['Tutaj jest artykuł 1', 'A to artykuł numer 2', 'Trzeci artykuł', 'Bla lbabla']})
 
-# Get user query
-# TODO: Implement GUI - Kacper
-# 1. Wczytanie inputu przez usera
-# 2. Ewentualny wybór sposobów semantyzacji
-# 3. Wyświetlenie wyników
-# W pliku parameters.py są aktualnie zaimplementowane metody:
-# tokenizacji, wektoryzacji, semantyzacji i obliczenia podobieństwa
-# Można w menu zaimplementować ich wybór
-# ewentualnie można też dodać opcję ponownego preprocessingu i dodania do bazy
-#    - jeśli dodamy dodatkowe artykuły do bazy/folderu
-# tokenizer_choice = TOKENIZER
-# vectorizer_choice = VECTORIZER
-# semantic_method_choice = SEMANTIZATOR
-# similarity_metric_choice = SIMILARITY_METRIC
-# num_topic = NUM_TOPICS
-
 tokenizer_choice = TOKENIZERS[0]
 vectorizer_choice = VECTORIZERS[0]
 semantic_method_choice = SEMANTIZATORS[0]
