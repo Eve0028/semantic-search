528a42b5835cac68491cb8528286ce1cf28950a4
Eve0028 <sylwia14082@gmail.com>
2023-12-18 18:02:15 +0100

    refactor: testing by 10 best articles

--- tests.py
+++ tests.py
@@ -2,8 +2,8 @@ from sklearn.metrics import precision_score, recall_score, f1_score
 import csv
 import pandas as pd
 
-from search_engine.parameters import STEMMERS, VECTORIZERS, SEMANTIZATORS, TOKENIZERS, NUM_TOPICS, SIMILARITY_METRICS, \
-    CSV_FILE, TEST_FILE, DIR_FILES
+from search_engine.parameters import STEMMERS, VECTORIZERS, SEMANTIZATORS, TOKENIZERS, DISTANCES_METRICS, \
+    CSV_FILE, TEST_FILE, DIR_FILES, NUMS_TOPICS
 from search_engine.semantic_search import search
 
 
@@ -27,65 +27,104 @@ def write_results_to_csv(results, output_file):
 
 
 # Funkcja do obliczania metryk dla różnych parametrów
-def calculate_metrics_for_parameters(query, articles, true_labels, output_file):
+def calculate_metrics_for_parameters(queries, labels, articles: pd.Series, output_file):
     results = []
 
-    for stemmer in STEMMERS:
-        for tokenizer in TOKENIZERS:
-            for vectorizer in VECTORIZERS:
-                for semantizator in SEMANTIZATORS:
-                    for num_topics in NUM_TOPICS:
-                        for similarity_metric in SIMILARITY_METRICS:
-                            # Wywołaj funkcję search z odpowiednimi parametrami
-                            similarities = search(query, articles, true_labels, tokenizer=tokenizer,
-                                                  vector_method=vectorizer,
-                                                  semantic_method=semantizator, num_topics=num_topics,
-                                                  similarity_metric=similarity_metric)
+    for i in range(len(queries)):
+        query = queries[i]
+        true_labels = labels[i]
 
-                            # Przekształć wyniki na etykiety binarne
-                            threshold = 0.0
-                            predicted_labels = [1 if similarity > threshold else 0 for similarity in similarities]
+        for stemmer in STEMMERS:
+            for tokenizer in TOKENIZERS:
+                for vectorizer in VECTORIZERS:
+                    for semantizator in SEMANTIZATORS:
+                        for num_topics in NUMS_TOPICS:
+                            for similarity_metric in DISTANCES_METRICS:
+                                # Wywołaj funkcję search z odpowiednimi parametrami
+                                distances = search(query, articles, tokenizer=tokenizer,
+                                                   vector_method=vectorizer,
+                                                   semantic_method=semantizator, num_topics=num_topics,
+                                                   similarity_metric=similarity_metric)
 
-                            # Oblicz metryki
-                            precision, recall, f1 = calculate_metrics(true_labels, predicted_labels)
+                                sorted_distances = sorted(distances)
 
-                            # Zapisz wyniki
-                            result = {
-                                'Stemmer': stemmer,
-                                'Tokenizer': tokenizer,
-                                'Vectorizer': vectorizer,
-                                'Semantizator': semantizator,
-                                'Num_Topics': num_topics,
-                                'Similarity_Metric': similarity_metric,
-                                'Precision': precision,
-                                'Recall': recall,
-                                'F1': f1
-                            }
+                                # Przekształć wyniki na etykiety binarne
+                                # threshold = 0.0
+                                threshold_idx = 10
+                                predicted_labels = [1 if distance < sorted_distances[threshold_idx] else 0 for distance
+                                                    in distances]
 
-                            results.append(result)
+                                # Oblicz metryki
+                                precision, recall, f1 = calculate_metrics(true_labels, predicted_labels)
+
+                                # Zapisz wyniki
+                                result = {
+                                    'Query': query,
+                                    'Stemmer': stemmer,
+                                    'Tokenizer': tokenizer,
+                                    'Vectorizer': vectorizer,
+                                    'Semantizator': semantizator,
+                                    'Num_Topics': num_topics,
+                                    'Similarity_Metric': similarity_metric,
+                                    'Precision': precision,
+                                    'Recall': recall,
+                                    'F1': f1
+                                }
+
+                                results.append(result)
 
     # Zapisz wyniki do pliku CSV
     write_results_to_csv(results, output_file)
 
 
 # Przykładowe dane testowe
-user_query = 'asthma in children'
 articles = pd.read_csv(f'{DIR_FILES}/{CSV_FILE}')
 results_file = f'{TEST_FILE}'
-true_labels = []
 
 # pd.set_option('display.max_colwidth', None)
+# print(articles['title'])
 # print(articles.loc[articles['title'] == 'Acute severe asthma'])
 
-query_results = [{
-    'query': 'asthma in children',
-    'titles': [],
-    'labels': []
-}, {
+google_results = [
+    {
+        'query': 'asthma in children',
+        'titles': {'Asthma', 'Epidemiology of asthma', 'Acute severe asthma', 'Asthma trigger', 'Allergies in children',
+                   'Pathophysiology of asthma', 'Asthma-related microbes', 'Reactive airway disease', 'Bronchiolitis',
+                   'Wheeze'}
+    },
+    {
+        'query': 'asthma inhaler types',
+        'titles': {'Inhaler', 'Dry-powder inhaler', 'Metered-dose inhaler', 'Salbutamol', 'Formoterol',
+                   'Pulmonary drug delivery', 'Bronchodilator', 'Asthma', 'Budesonide/formoterol',
+                   'Inhaler spacer'}
+    }]
 
-}, {
+query_results = []
 
-}]
+for result in google_results:
+    query = result['query']
+    google_results_titles = result['titles']
+    query_results_titles = articles['title'].tolist()
+
+    labels = tuple(map(lambda article_title: article_title in google_results_titles, query_results_titles))
+
+    query_results.append({
+        'query': query,
+        'titles': query_results_titles,
+        'labels': labels
+    })
+
+print(query_results[0]['titles'])
+df_query_results = pd.DataFrame(query_results)
+
+# df_google_results = pd.DataFrame(google_results)
+# queries = df_google_results['query'].tolist()
+# google_titles = df_google_results['titles'].tolist()
+# articles = pd.read_csv(f'{DIR_FILES}/{CSV_FILE}')
+# articles = articles['clean_content']
 
 # Wywołaj funkcję do obliczania metryk dla różnych parametrów
-# calculate_metrics_for_parameters(user_query, articles, true_labels, output_file=results_file)
+calculate_metrics_for_parameters(df_query_results['query'].tolist(),
+                                 df_query_results['labels'].tolist(),
+                                 articles['clean_content'],
+                                 output_file=results_file)
